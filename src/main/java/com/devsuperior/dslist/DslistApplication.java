85d5ce2e0a471cdccfe0c566242872e462ce492b
Mislaine <a.s.mislaine@gmail.com>
2023-07-11 11:38:35 -0300

    Project created

---
+++ src/main/java/com/devsuperior/dslist/DslistApplication.java
@@ -0,0 +1,18 @@
+package com.devsuperior.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class DslistApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication app = new SpringApplication( DslistApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+		app.run(args);
+	}
+
+}
